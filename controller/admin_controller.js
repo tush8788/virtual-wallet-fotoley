65f7953a59bd0339c21f2ffe8ef7410f37561f73
tush87 <tusharj8788@gmail.com>
2023-04-27 21:34:32 +0530

    add admin dashboard and its features

---
+++ controller/admin_controller.js
@@ -0,0 +1,15 @@
+const UserDB = require('../models/user');
+
+module.exports.dashboard=async function(req,res){
+    try{
+        let users = await UserDB.find({ email: { $ne: req.user.email }, isAdmin: { $ne: true } });
+        // console.log(users);
+        return res.render('./admin/dashboard',{
+            title:"dashboard",
+            users
+        })
+    }
+    catch(err){
+
+    }
+}
--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const authController = require('../controller/auth_controller');
+const adminController = require('../controller/admin_controller');
 
 //signin page
 router.get('/signin',(req,res,next)=>{
@@ -23,4 +24,6 @@ router.post('/create',authController.create);
 //create session
 router.post('/create-session',passport.authenticate('local',{failureRedirect:"/admin/signin"}),authController.createSession);
 
+//admin dashboard
+router.get('/dashboard',passport.checkAuthentication,passport.isAdminOrNot,adminController.dashboard)
 module.exports = router;
